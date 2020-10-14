5967e02b249181f72c14ae5df9d1e0db2b7ba00c
Gabriel Aranda <per.gabrielaranda@gmail.com>
2020-10-14 16:18:07 -0300

    Created first version of orphanage registration + typeorm orphanage model

--- server/src/database/migrations/1602695248298-create_orphanages.ts
+++ server/src/database/migrations/1602695248298-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602695248298 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,40 @@
-import express from 'express'
+import express, { request, response } from 'express'
+import { getRepository } from 'typeorm'
+
+import Orphanage from './models/Orphanage'
 
 import './database/connection'
 
 const app = express()
+
+app.use(express.json())
+
+app.post('/orphanages', async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = req.body
+
+  const orphanagesRepository = getRepository(Orphanage)
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  })
+
+  await orphanagesRepository.save(orphanage)
+
+  res.json('Success')
+})
 
 app.listen(3333)
